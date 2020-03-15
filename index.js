9d287fc45510ebcf3ce1eb79a0e76363fb8e8b24
lephuclam97 <lamboyboy20@gmail.com>
2020-03-15 21:39:43 +0700

    Init my resume

---
+++ index.js
@@ -0,0 +1,27 @@
+var arrSide = ['brand-side-1','brand-side-2','brand-side-3','brand-side-4']
+
+// function scrollPage(y) {
+//    var ele = document.getElementsByClassName('brand-side-bar')
+// //    ele.classListadd('hellos')
+//     window.scrollTo(0,y);
+// }
+
+$(document).ready(function (){
+    $('#brand-side-1').css('background','#ffdf00')
+    $('.brand-side-2-content').css('display','none')
+    $('.brand-side-3-content').css('display','none')
+    $('.brand-side-4-content').css('display','none')
+    
+    $('.brand-side-bar').on('click', function() {
+        var panelId = $(this).attr('data-panelid')
+        $('#'+panelId).css('background','#ffdf00')
+        $('.'+panelId + '-content').css('display','block')
+
+        for(var i in arrSide){
+            if(arrSide[i] !== panelId){
+              $('#'+arrSide[i]).css('background','white')
+              $('.'+arrSide[i]+'-content').css('display','none')
+            }
+        }
+    })
+})
