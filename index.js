d58c42aaad3ac2a7cd4afac7a04e8be33826a72c
Anh Tran <anh.tran@syntrinos.com.my>
2020-05-25 22:28:35 +0700

    Update Responsive

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var arrSide = ['brand-side-1','brand-side-2','brand-side-3','brand-side-4']
+var arrSide = ["brand-side-1", "brand-side-2", "brand-side-3", "brand-side-4"];
 
 // function scrollPage(y) {
 //    var ele = document.getElementsByClassName('brand-side-bar')
@@ -6,25 +6,24 @@ var arrSide = ['brand-side-1','brand-side-2','brand-side-3','brand-side-4']
 //     window.scrollTo(0,y);
 // }
 
-$(document).ready(function (){
+$(document).ready(function () {
+  // $('.info-box-item').carousel();
 
-    // $('.info-box-item').carousel();
+  $("#brand-side-1").css("background", "#ffdf00");
+  $(".brand-side-2-content").css("display", "none");
+  $(".brand-side-3-content").css("display", "none");
+  $(".brand-side-4-content").css("display", "none");
 
-    $('#brand-side-1').css('background','#ffdf00')
-    $('.brand-side-2-content').css('display','none')
-    $('.brand-side-3-content').css('display','none')
-    $('.brand-side-4-content').css('display','none')
-    
-    $('.brand-side-bar').on('click', function() {
-        var panelId = $(this).attr('data-panelid')
-        $('#'+panelId).css('background','#ffdf00')
-        $('.'+panelId + '-content').css('display','block')
+  $(".brand-side-bar").on("click", function () {
+    var panelId = $(this).attr("data-panelid");
+    $("#" + panelId).css("background", "#ffdf00");
+    $("." + panelId + "-content").css("display", "block");
 
-        for(var i in arrSide){
-            if(arrSide[i] !== panelId){
-              $('#'+arrSide[i]).css('background','white')
-              $('.'+arrSide[i]+'-content').css('display','none')
-            }
-        }
-    })
-})
+    for (var i in arrSide) {
+      if (arrSide[i] !== panelId) {
+        $("#" + arrSide[i]).css("background", "white");
+        $("." + arrSide[i] + "-content").css("display", "none");
+      }
+    }
+  });
+});
